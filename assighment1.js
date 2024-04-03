9f17d56871037b6f9dc5e0d4007958117e6f651c
genet kahsay <genetk@gmail.com>
2024-04-03 00:44:43 -0500

    hw

---
+++ assighment1.js
@@ -0,0 +1,17 @@
+function sum(arr){
+    return result = arr.filter(element=>element>20)
+   .reduce((accum,currntvalue)=>
+      accum+currntvalue,0)
+      
+}
+
+const getNewArray=(str)=>{
+    return str.filter((ele=>ele.length>=5&& ele.includes('a')))
+}
+
+
+const concat=(str,...arr)=>{
+    let result =[...str,...arr];
+    return result 
+}
+console.log(concat('hi', [1, 2, 3], ['Hello', 'world']));
