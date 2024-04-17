7e2b1306970c4d7c1c471b259449715e7a29200e
genet kahsay <genetk@gmail.com>
2024-04-17 00:12:20 -0500

    git pub-subscribe

---
+++ workshop4-github/pattern/src/App.tsx
@@ -0,0 +1,14 @@
+import Search from "./componenet/search";
+import List from "./componenet/list";
+
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+      <List />
+    </div>
+  );
+}
+export default App;
---
+++ workshop4-github/pattern/src/componenet/types/search.type.ts
@@ -0,0 +1,9 @@
+
+import  User from "./user"
+
+export default interface SearchReponse {
+    isFirst: boolean,
+    isLoading: boolean,
+    isError: boolean,
+    users: User[]
+}
